32a2f7b540c771ac3b7f2350e5a0d119e3bf7fa9
Shravya Unukuri <shravyaunukuri0109@gmail.com>
2024-12-03 20:34:59 +0530

    first commit

---
+++ send_email.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "office@blackvision.in"; // Enter your email address here
+    $subject = "Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+
+    if (mail($to, $subject, $body)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again.";
+    }
+}
+?>
